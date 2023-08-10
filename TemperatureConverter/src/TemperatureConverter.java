db5f1b2acf5f60ea166b3324a1e60355c176d197
Poojabbofficial <poojabbofficial1991@gmail.com>
2023-08-10 13:37:57 +0530

    PROGRAMS ON METHODS
    ALL PROGRAMS BELONGS TO CONCEPT CALLED METHODS

---
+++ TemperatureConverter/src/TemperatureConverter.java
@@ -0,0 +1,10 @@
+
+public class TemperatureConverter {  
+
+    public double convertFahrenheitToCelsius(double fahrenheit)
+    {
+    	double celcius=(fahrenheit-32)*5/9;
+    	return celcius;
+    }
+
+}  
---
+++ TemperatureConverter/src/TemperatureConverterApp.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TemperatureConverterApp {
+public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	System.out.println("Enter the temperature in degree-F");
+	double f=scan.nextDouble();
+	TemperatureConverter temperatureConverter=new TemperatureConverter();
+	double tempincelcius=temperatureConverter.convertFahrenheitToCelsius(f);
+	System.out.println(tempincelcius);
+	
+
+}
+}
